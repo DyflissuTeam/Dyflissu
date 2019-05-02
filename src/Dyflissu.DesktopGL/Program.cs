ac55f6273d7ac7089088fa8fb016e4549d74281e
YogurtTheHorse <yegorf1@gmail.com>
2019-05-02 16:36:39 +0300

    Add empty stubs for projects

---
+++ src/Dyflissu.DesktopGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dyflissu.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}

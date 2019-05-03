be15c20a136b73632b669805cbf6f251fb9e38e7
YogurtTheHorse <yegorf1@gmail.com>
2019-05-03 19:16:42 +0300

    Set default Restitution to zero for bodies

--- src/Dyflissu.Physics/Body.cs
+++ src/Dyflissu.Physics/Body.cs
@@ -14,7 +14,7 @@ namespace Dyflissu.Physics
         public Vector2 Acceleration { get; set; }
 
         public float Mass { get; set; } = 1f;
-        public float Restitution { get; set; } = 20f;
+        public float Restitution { get; set; } = 0;
         
         public float InverseMass => Mass == 0 ? 0 : 1f / Mass;
 

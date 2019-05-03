649bcd994acdbf5879a21f8679639f74ac39c556
YogurtTheHorse <yegorf1@gmail.com>
2019-05-03 15:02:29 +0300

    Add physics math class

---
+++ src/Dyflissu.Physics/PhysicsMath.cs
@@ -0,0 +1,12 @@
+using Dyflissu.Primitives;
+
+namespace Dyflissu.Physics
+{
+    public class PhysicsMath
+    {
+        public static float DotProduct(Vector2 a, Vector2 b)
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+    }
+}
